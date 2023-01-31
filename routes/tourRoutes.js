03ed8edf567ceccee01d05b95097116b5c32df6e
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-01-31 16:28:54 +0700

    updated tour controller, model and routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const tourController = require('../controllers/tourController');
 
+router.route('/top-5-cheap').get();
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router
   .route('/:id')
